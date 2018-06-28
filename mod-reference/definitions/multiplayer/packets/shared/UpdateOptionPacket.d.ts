f6c80874ea80e5ac17191b38c410abcf89660e47
Gary Wilber <Spacetech326@gmail.com>
2018-06-27 19:42:45 -0700

    Fixing submodule

--- mod-reference/definitions/multiplayer/packets/shared/UpdateOptionPacket.d.ts
+++
@@ -1,8 +0,0 @@
-import PlayerTargetedSharedPacket from "multiplayer/packets/PlayerTargetedSharedPacket";
-import { IOptions } from "save/data/ISaveDataGlobal";
-export default class UpdateOptionPacket extends PlayerTargetedSharedPacket {
-    id: keyof IOptions;
-    value: boolean | number;
-    isAllowedWhenPaused(): boolean;
-    process(): void;
-}
